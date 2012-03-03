2dfa1a644fa3efd6731a9a5f5e8cbfa76cc8f8d2
Armin K <akiany@gmail.com>
2012-03-03 15:24:17 -0500

    Changed the links to scroll smoothly to their destination div#id

--- media/base.js
+++ media/base.js
@@ -6,4 +6,23 @@ function flip_cursor(){
 
 $(document).ready(function(){
     flip_cursor();
+    
+    // smooth scroll for links:
+    $('nav a, .back-up').click(function(){
+        smooth_scroll(this.href.split('#')[1]);
+    });
 });
+
+
+// Animates a smooth scroll to #id
+function smooth_scroll(id){
+    $('html,body').animate({
+        scrollTop:$('#' + id).offset().top
+        }, 700);
+    
+    setTimeout(function(){
+        window.location.hash = '#' + id;
+        }, 700);
+    
+    return false;
+}
